bf379ba5dbd9963fa2f02b2a23ce9deb19036aff
Sakshi Wanode <110322000+Sakshiwanode@users.noreply.github.com>
2024-10-03 17:53:44 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({ navigation }: any) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        color={'green'}
+        onPress={() => navigation.navigate('Details')}
+       /> 
+
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+       
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
